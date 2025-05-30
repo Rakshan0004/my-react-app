dd3525466a1cd8915b8b40830795d41b7a2e4550
Chintala Rakshan <136550641+Rakshan0004@users.noreply.github.com>
2025-05-30 12:50:54 +0530

    /.

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,22 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar"
+import BlogList from "./components/BlogList";
 
 const Home = () => {
 
-    const handleClick = () => {
-        console.log('hello');
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target)
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: ' lorem ipsum...', author: 'mario', id:1 },
+        { title: 'My travel blog', 
+        body: 'Exploring the world one step at a time...', 
+        author: 'luigi', id: 2 },
+        { title: 'Tech Tips', 
+        body: 'Sharing insights and hacks for tech enthusiasts...', 
+        author: 'peach', id: 3 }
+    ]);
 
     return (
-        <div className="Home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('rakshan', e)}>Click me again</button>
+        <div className="home">
+            <BlogList blogs={blogs}/>
         </div>
     )
 }
