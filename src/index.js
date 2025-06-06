e632286f0136a25fb1aa9c8dc10271f0db93003b
Chintala Rakshan <136550641+Rakshan0004@users.noreply.github.com>
2025-06-06 15:18:33 +0530

    .

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import ProductApp from './components/ProductApp';
 import EventTest from './components/EventTest';
 import Button from './components/EventHandle';
 import Ninja from './components/Ninja.jsx';
+import { BrowserRouter } from 'react-router-dom'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    
     {/* <ShoppingList />
       <Counter /> */}
 
@@ -23,8 +27,4 @@ root.render(
     {/* <Ninja /> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
